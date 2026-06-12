a39300664f294e43b6f55fff4a80e1f4b0579d5d
Dhrubba <dhrubajyotibhuyan9075@gmail.com>
2026-06-13 01:56:08 +0530

    routes added

---
+++ backend/Routes/notesRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    res.status(200).send("You just fetched all the notes");
+});
+
+router.post("/", (req,res) => {
+    res.status(210).json({
+        message: "Note has been created successfully"
+    })
+})
+
+router.put("/:id", (req,res) => {
+    res.status(200).json({
+        message: "Note updated successfully"
+    })
+})
+
+
+
+router.delete("/:id", (req,res) => {
+    res.status(200).json({
+        message: "Note deleted successfully"
+    })
+})
+
+
+export default router;
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+import notesRoutes from "./Routes/notesRoutes.js";
 
 const app = express();
 
-app.get("/api/notes", (req,res) => {
-    res.send("You got 5 notes");
-});
+app.use("/api/notes")
+
+
 
 app.listen(5001, () => {
     console.log('Server is running....');
