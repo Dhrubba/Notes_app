894e3f5845af942e632ae013860a0ac215d26a5d
Dhrubba <dhrubajyotibhuyan9075@gmail.com>
2026-06-18 19:42:53 +0530

    controllers

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,12 @@
-export function getAllNotes(req,res) {
-    res.status(200).send("You just fetched all the notes");
+import Note from "../models/Note.js";
+export async function getAllNotes(req,res) {
+    try{
+        const notes = await Note.find();
+        res.status(200).json(notes); 
+    } catch (error) {
+        console.error("Error fetching notes:", error);
+        res.status(500).json({ message:"Internal Server Error"});
+    }
 }
 
 export function createNote(req,res) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,10 @@ import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 
 
+console.log(process.env.MONGO_URI); // Log the MongoDB URI to verify it's being read correctly
+
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
@@ -14,6 +17,6 @@ app.use("/api/notes" , notesRoutes);
 
 
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
     console.log('Server is running....');
 });
