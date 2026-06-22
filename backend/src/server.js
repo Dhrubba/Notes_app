4b9a7a7dfadd00fd0a9d5a7a72d875b13ff2d761
Dhrubba <dhrubajyotibhuyan9075@gmail.com>
2026-06-23 04:09:00 +0530

    Rate limiter added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from 'express';
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 
 console.log(process.env.MONGO_URI); // Log the MongoDB URI to verify it's being read correctly
@@ -12,6 +13,17 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 connectDB();
+
+//middleware
+app.use(express.json());
+app.use(rateLimiter);
+
+//simple custom middleware
+// app.use((req, res, next) =>{
+//     console.log('request method:', req.method, 'request url:', req.url);
+//     next();
+// });
+
 
 app.use("/api/notes" , notesRoutes);
 
